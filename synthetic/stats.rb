08251fe366f04f5399940cf67cb7523568900c91
Haitham Gabr <hgabr@cise.ufl.edu>
2013-08-20 11:54:18 -0400

    minor update to synthetic stats script

--- synthetic/stats.rb
+++ synthetic/stats.rb
@@ -1,14 +1,14 @@
-dir, output = ARGV
-size = dir.split("/").last.split("-"),first.to_i
+dir = ARGV[0]
+size = dir.split("/").last.split("-").first.to_i
 
-open output, "w" do |f|
+open "#{dir}/stats.out", "w" do |f|
 	0.upto(size-1) do |s|
 		0.upto(size-1) do |t|
 			next if s == t
 			puts "../preach #{dir}/network.txt node#{s}.txt node#{t}.txt"
 			output = `../preach #{dir}/network.txt node#{s}.txt node#{t}.txt`
 			f.puts "#{s}  #{t}  #{output.strip}"
-			#f.puts "../preach #{network} #{source} #{target}"
+			#f.puts "../preach #{dir}/network.txt node#{s}.txt node#{t}.txt"
 		end
 	end
 end
