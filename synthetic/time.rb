66b4cf423588c9f2be04446c426b4c948fd23833
Haitham Gabr <hgabr@cise.ufl.edu>
2013-08-23 11:06:58 -0400

    ready for synthetic performance testing

---
+++ synthetic/time.rb
@@ -0,0 +1,16 @@
+dir = ARGV[0]
+size = dir.split("/").last.split("-").first.to_i
+
+open "#{dir}/stats.out", "w" do |f|
+	0.upto(size-1) do |s|
+		0.upto(size-1) do |t|
+			next if s == t
+			puts `../stats #{dir}/network.txt #{dir}/node#{s}.txt #{dir}/node#{t}.txt`
+			output = `time -p ../preach #{dir}/network.txt #{dir}/node#{s}.txt #{dir}/node#{t}.txt`
+			time = output.split("real ").last.split(/\s+/).first
+			f.puts "#{s}  #{t}  #{time.strip}"
+			#f.puts "../preach #{dir}/network.txt #{dir}/node#{s}.txt #{dir}/node#{t}.txt"
+		end
+	end
+end
+
