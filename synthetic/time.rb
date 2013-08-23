fcbf01209cd078f1dbe653b6076a75a57f2c7e5f
Haitham Gabr <hgabr@cise.ufl.edu>
2013-08-23 16:07:48 -0400

    debugging synthetic/time.rb

--- synthetic/time.rb
+++ synthetic/time.rb
@@ -6,10 +6,8 @@ open "#{dir}/time.out", "w" do |f|
 		0.upto(size-1) do |t|
 			next if s == t
 			puts `../stats #{dir}/network.txt #{dir}/node#{s}.txt #{dir}/node#{t}.txt`
-			time = `echo -n #{"`(time -p ../preach #{dir}/network.txt #{dir}/node#{s}.txt #{dir}/node#{t}.txt) 2>&1 | grep real | tr -d '\n' | cut -d" " -f2`"}`
-			#time = output.split("real ").last.split(/\s+/).first
+			time = `(time -p ../preach #{dir}/network.txt #{dir}/node#{s}.txt #{dir}/node#{t}.txt) 2>&1 | grep real | tr -d '\n' | cut -d" " -f2`
 			f.puts "#{s}  #{t}  #{time.strip}"
-			#f.puts "../preach #{dir}/network.txt #{dir}/node#{s}.txt #{dir}/node#{t}.txt"
 		end
 	end
 end
