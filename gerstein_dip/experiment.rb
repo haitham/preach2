3de94950b017574aa0aa1242f602fbea6cbd9a15
Haitham Gabr <hgabr@cise.ufl.edu>
2013-08-26 17:36:03 -0400

    debugging gerstein_dip experiment.rb

--- gerstein_dip/experiment.rb
+++ gerstein_dip/experiment.rb
@@ -6,8 +6,8 @@ open output, "w" do |f|
 	sources.each do |source|
 		targets.each do |target|
 			stat = `../stats #{network} #{source} #{target}`
-			next if stat.split.size == 4
 			puts "#{source}  #{target}  #{stat.strip}"
+			next if stat.split.size == 4
 			output = `../preach #{network} #{source} #{target}`
 			f.puts "#{source}  #{target}  #{output.strip.split.last}"
 		end
