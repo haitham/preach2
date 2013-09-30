59180e994928b380b7cadace4ecae8e81783c9e4
Haitham Gabr <hgabr@cise.ufl.edu>
2013-09-30 18:52:58 -0400

    adding centrality networks

---
+++ gerstein_dip/centrality/experiment.rb
@@ -0,0 +1,38 @@
+#argument example: "type9" or "type_GSE42414_0"
+dataset = ARGV[0]
+sources = Dir.glob("source*.txt").map{|file| file.split("source").last.split(".").first.to_i}.sort
+targets = Dir.glob("target*.txt").map{|file| file.split("target").last.split(".").first.to_i}.sort
+reference = {}
+
+sources.each do |source|
+	reference[source] = {}
+	targets.each do |target|
+		output = `../../preach #{dataset}.txt source#{source}.txt target#{target}.txt`
+		reference[source][target] = output.strip.split.last.to_f
+		puts "Ran #{dataset} reference from #{source} to #{target}: #{reference[source][target]}"
+	end
+end
+
+nodes = Dir.glob("#{dataset}.missing_*.txt").map{|file| file.split("missing_").last.split(".txt").first}
+values = {}
+nodes.each do |node|
+	values[node] = 0.0
+	sources.each do |source|
+		targets.each do |target|
+			output = `../../preach #{dataset}.missing_#{node}.txt source#{source}.txt target#{target}.txt`
+			new_value = output.strip.split.last.to_f
+			puts "Ran missing_#{node} from #{source} to #{target}: #{new_value}"
+			values[node] = values[node] + reference[source][target] - new_value
+		end
+	end
+end
+
+open "#{dataset}.sorted.out", "w" do |f|
+	values.map{|k,v| [k,v]}.sort{|a,b| b[1] <=> a[1]}.each{|p| f.puts "#{sprintf "%-12f", p[1]}#{"  "}#{p[0]}"}
+end
+
+open "#{dataset}.out", "w" do |f|
+	nodes.each do |node|
+		f.puts "#{sprintf "%-10s", node}#{values[node]}"
+	end
+end
---
+++ gerstein_dip/centrality/generate.rb
@@ -0,0 +1,23 @@
+datasets = ARGV
+edges = []
+nodes = []
+
+datasets.each do |dataset|
+	open dataset do |f|
+		until (line = f.gets).nil?
+			next if line.strip.empty?
+			parts = line.split.map{|p| p.strip}
+			edges << parts
+			nodes << parts[0] unless nodes.include? parts[0]
+			nodes << parts[1] unless nodes.include? parts[1]
+		end
+	end
+
+	nodes.each do |node|
+		open dataset.gsub(".txt", ".missing_#{node}.txt"), "w" do |f|
+			edges.each do |edge|
+				f.puts edge.join " " unless edge.include? node
+			end
+		end
+	end
+end
