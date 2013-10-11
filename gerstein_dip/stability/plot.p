38312236027f49bf0de9051f98935a5689797dc5
Haitham Gabr <hgabr@cise.ufl.edu>
2013-10-11 17:16:58 -0400

    stability figures

--- gerstein_dip/stability/plot.p
+++ gerstein_dip/stability/plot.p
@@ -50,3 +50,18 @@ plot 'avg_abs.out' using 1:2 w lp lw 2  title 'Hyperdiploid',\
 set output
 !epstopdf avg_abs_min.eps
 
+set key left
+set output 'avg_abs_2.eps'
+plot 'avg_abs_2.out' using 1:2 w lp lw 2  title 'Hyperdiploid',\
+	 'avg_abs_2.out' using 1:3 w lp lw 2  title 'TCF3-PBX1',\
+	 'avg_abs_2.out' using 1:4 w lp lw 2  title 'ETV6\_RUNX1',\
+	 'avg_abs_2.out' using 1:5 w lp lw 2  title 'MLL',\
+	 'avg_abs_2.out' using 1:6 w lp lw 2  title 'Ph',\
+	 'avg_abs_2.out' using 1:7 w lp lw 2  title 'Hypo',\
+	 'avg_abs_2.out' using 1:8 w lp lw 2  title 'Other',\
+	 'avg_abs_2.out' using 1:9 w lp lw 2  title 'T-ALL',\
+	 'avg_abs_2.out' using 1:10 w lp lw 2  title 'Control\_HSC',\
+	 'avg_abs_2.out' using 1:11 w lp lw 2  title 'Control\_lymphoid',\
+	 'avg_abs_2.out' using 1:12 w lp lw 2  title 'Control\_myeloid'
+set output
+!epstopdf avg_abs_2.eps
--- gerstein_dip/stability/summarize.rb
+++ gerstein_dip/stability/summarize.rb
@@ -13,36 +13,27 @@ deltas = ["0.05", "0.1", "0.15", "0.2", "0.25", "0.3"]
 datasets = ARGV
 
 avg = {}
-abs = {}
 datasets.each do |dataset|
 	open dataset do |f|
 		values = deltas.map{|d| 0.0}
-		abs_values = deltas.map{|d| 0.0}
 		count = 0
 		f.gets #skip header
 		until (line = f.gets).nil?
-			line.strip.split.last(deltas.size).each_with_index do |v, i|
-				values[i] = values[i] + v.to_f
-				abs_values[i] = abs_values[i] + v.to_f.abs
+			parts = line.strip.split
+			ref = parts[3].to_f
+			parts.last(deltas.size).each_with_index do |v, i|
+				values[i] = values[i] + (v.to_f - ref).abs
 			end
 			count = count + 1
 		end
 		avg[dataset.split("type").last.split(".out").first] = values.map{|v| v/count}
-		abs[dataset.split("type").last.split(".out").first] = abs_values.map{|v| v/count}
 	end
 end
 
-open "avg.out", "w" do |f|
+open "avg_abs_2.out", "w" do |f|
 	f.puts "#{sprintf "%-8s", "#delta"}#{avg.keys.map{|t| sprintf "%-18s", typenames[t]}.join}"
 	deltas.each_with_index do |d, i|
 		f.puts "#{sprintf "%-8s", d}#{avg.keys.map{|t| sprintf "%-18f", avg[t][i].round(12)}.join}"
-	end
-end
-
-open "avg_abs.out", "w" do |f|
-	f.puts "#{sprintf "%-8s", "#delta"}#{abs.keys.map{|t| sprintf "%-18s", typenames[t]}.join}"
-	deltas.each_with_index do |d, i|
-		f.puts "#{sprintf "%-8s", d}#{abs.keys.map{|t| sprintf "%-18f", abs[t][i].round(12)}.join}"
 	end
 end
 
